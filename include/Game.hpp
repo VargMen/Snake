aa5c93e9a28fb4eb090482b4d45f5d76d03c4cd5
Danylo Uchen <unoduchaso@gmail.com>
2024-01-31 19:17:41 +0200

    Rearange functions
    Make it more good looking in Game.hpp

--- include/Game.hpp
+++ include/Game.hpp
@@ -21,52 +21,40 @@ public:
     ~Game() { endwin(); }
 
 private:
+    bool isOver() const;
 
+    void spawnSnake();
+    void spawnFood();
+    void spawnOnBoard(const Point& point, char symbol);
+
+
+    void clearWindow();
+    void refreshWindow();
+
+    void printScore();
+    void printMessageToPlayAgain();
+
+
+    static Snake::Direction parseToDirection(int ch);
+    Snake::Direction getNewDirection();
+
+    int getAnswer();
+
+    void displayState();
+    void updateState();
+
+    void addSpeed();
+
+    void stopGame();
+    void restartGame();
+
+private:
     Board m_board{setting::mapPath};
     Snake m_snake{Point{3, 3}, Snake::Direction::up};
     Food m_food{Point{5, 5} };
     WINDOW * m_winGame{};
     WINDOW * m_winScore{};
     bool loseFlag{false};
-
-    bool isOver() const;
-
-
-    void spawnSnake();
-
-    void spawnFood();
-
-    void spawnOnBoard(const Point& point, char symbol);
-
-
-    void clearWindow();
-
-    void refreshWindow();
-
-    void printScore();
-
-    void printMessageToPlayAgain();
-
-
-    static Snake::Direction parseToDirection(int ch);
-
-    Snake::Direction getNewDirection();
-
-
-    int getAnswer();
-
-
-    void displayState();
-
-    void updateState();
-
-
-    void addSpeed();
-
-    void stopGame();
-
-
-    void restartGame();
 };
 
 
